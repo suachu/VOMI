d7b7d238c47234081d5d3ea905d38e86aa90a503
Sua Chu <suachu@Suas-MacBook-Air.local>
2026-02-02 17:32:29 +0900

    colors

---
+++ lib/core/theme/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Colors {
+  static const Color primary = Color.fromRGBO(172, 215, 230, 1);  // Main brand color
+  static const Color secondary = Color.fromRGBO(243, 252, 255, 1);
+  static const Color background = Color.fromRGBO(r, g, b, opacity);
+  static const Color textDark = Color.fromRGBO(r, g, b, opacity);
+  static const Color textLight = Color(0xFF888888);
+  
+}
