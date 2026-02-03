de37db4d7967de908e70780dea767b6bc6a2fa41
Sua Chu <suachu@Suas-MacBook-Air.local>
2026-02-03 15:07:24 +0900

    restart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:vomi/views/character/character_select_page.dart';
 import 'package:vomi/core/theme/theme.dart';
 
 void main() {
@@ -13,7 +12,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: AppTheme.lightTheme,
-      home: const CharacterSelectPage(userName: '영준'),
     );
   }
 }
